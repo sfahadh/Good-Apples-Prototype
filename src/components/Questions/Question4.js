d209cd2f31f270bf5b8f40af673fdd6301685d69
Fahad Hussain <sfahadh14@gmail.com>
2019-05-19 16:50:33 -0400

    added radio buttons

--- src/components/Questions/Question4.js
+++ src/components/Questions/Question4.js
@@ -23,28 +23,26 @@ export default class Question4 extends Component {
     return (
       <div className="questions-page">
         <form className="question-form">
-          <h3>How much time are you willing to invest overall?</h3>
-          <h4>(you can select multiple choices)</h4>
+          <h3 >How much time are you willing to invest overall?</h3>
+          <h4></h4>
+          <br/>
           <div className="answer-choices"> 
-            <div className="checkbox">
-              <input type="checkbox" id="checkbox-12"/>
-              <label htmlFor="checkbox-12">
-                <span className="label-field">less than a month</span>
-              </label>
+            <div className="radio">
+              <input type="radio" id="radio-1" name="selector"/>
+                <label for="radio-1">Pizza</label>
+              <div class="check"></div>
             </div>
-
-            <div className="checkbox">
-              <input type="checkbox" id="checkbox-13"/>
-              <label htmlFor="checkbox-13">
-                <span className="label-field">less than a year</span>
-              </label>
+          
+            <div className="radio">
+              <input type="radio" id="radio-2" name="selector"/>
+                <label for="radio-2">Pizza</label>
+              <div class="check"></div>
             </div>
-
-            <div className="checkbox">
-              <input type="checkbox" id="checkbox-14"/>
-              <label htmlFor="checkbox-14">
-                <span className="label-field">more than a year</span>
-              </label>
+            
+            <div className="radio">
+              <input type="radio" id="radio-3" name="selector"/>
+                <label for="radio-3">Pizza</label>
+              <div class="check"></div>
             </div>
           </div>
           <button onClick={this.handleSubmit}>Submit</button>
