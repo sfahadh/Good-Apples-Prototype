b38377fc3c997d241725bf2a420ab48c66409e09
Fahad Hussain <sfahadh14@gmail.com>
2019-05-19 11:36:36 -0400

    made quiz page responsive

--- src/components/Quiz/Questions.js
+++ src/components/Quiz/Questions.js
@@ -2,38 +2,54 @@ import React, { Component } from 'react'
 import './Quiz.css'
 
 export default class componentName extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        console.log("submit")
+    }
+
+
   render() {
     return (
-      <div>
+      <div className="questions-page">
         <form className="question-form">
             <h3>What is your area of interest?</h3>
             <h4>(you can select multiple choices)</h4>
-            <div class="answer-choices"> 
-                <div class="checkbox">
+            <div className="answer-choices"> 
+                <div className="checkbox">
                     <input type="checkbox" id="checkbox-1"/>
-                    <label for="checkbox-1"><span id="business">Business</span></label>
+                    <label htmlFor="checkbox-1"><span id="business">Business</span></label>
                 </div>
 
-                <div class="checkbox">
+                <div className="checkbox">
                     <input type="checkbox" id="checkbox-2"/>
-                    <label for="checkbox-2"><span id="design">Design</span></label>
+                    <label htmlFor="checkbox-2"><span id="design">Design</span></label>
                 </div>
 
                 <div className="checkbox">
                     <input type="checkbox" id="checkbox-3"/>
-                    <label for="checkbox-3"><span id="culinary-arts">Culinary Arts</span></label>
+                    <label htmlFor="checkbox-3"><span id="culinary-arts">Culinary Arts</span></label>
                 </div>
 
                 <div className="checkbox">
                     <input type="checkbox" id="checkbox-4"/>
-                    <label for="checkbox-4"><span id="technology">Technology</span></label>
+                    <label htmlFor="checkbox-4"><span id="technology">Technology</span></label>
                 </div>
 
                 <div className="checkbox">
                     <input type="checkbox" id="checkbox-5"/>
-                    <label for="checkbox-5"><span id="vocational-training">Vocational Training</span></label>
+                    <label htmlFor="checkbox-5"><span id="vocational-training">Vocational Training</span></label>
                 </div> 
             </div>
+            <button onClick={this.handleSubmit}>Submit</button>
         </form>
       </div>
     )
