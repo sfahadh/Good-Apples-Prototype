4daa2c1f665237eb587d2a8014b59dfa5739f654
Fahad Hussain <sfahadh14@gmail.com>
2019-05-18 20:38:36 -0400

    added texts on bottom of home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,10 @@ export default class Home extends Component {
           <button>Find My Course</button>
         </div>
         <div className='second-image'></div>
-        <div className='third-image'></div>
+        <div className='third-image'>
+          <h2>What Good Apples does:</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+        </div>
       </div>
     )
   }
